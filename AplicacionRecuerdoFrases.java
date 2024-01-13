0ba6d0f08d7a1424da3056cec58182e6ec094806
Hualca22 <149343770+Hualca22@users.noreply.github.com>
2024-01-13 17:07:16 -0500

    Add files via upload

---
+++ AplicacionRecuerdoFrases.java
@@ -0,0 +1,7 @@
+import javax.swing.SwingUtilities;
+
+public class AplicacionRecuerdoFrases {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new InterfazDesafio().setVisible(true));
+    }
+}
